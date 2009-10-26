265175a666cd7bb6daf6126fc9c03af922ec6997
tomasorti <tomasorti@dc152cb4-b957-11de-92a3-373a1addbe3b>
2009-10-26 06:26:13 +0000

    Changed names. Added TAB and command validation.

--- ishell.cpp
+++ ishell.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstring>
+#include <map>
+
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
-#include <cstring>
+#include <signal.h>
 
 using namespace std;
 
@@ -16,41 +19,59 @@ class InteractiveShell
 {
 public:
 
-    InteractiveShell() : linePos_(0), cmdIndex_(0), rowIndex_(0), pageStep_(5)
+    InteractiveShell() : linePos_(0), cmdIndex_(0), popIndex_(0), pageStep_(5)
     {
     }
 
+    void init();
     void run();
 
 private:
     typedef unsigned int uint;
-    vector<char> line_;
-    uint linePos_;
+    vector<char> line_; // Holds the current command line contents
+    uint linePos_; // Position in the command line
 
-    vector<string> cmdHistory_;
-    uint cmdIndex_;
-    uint rowIndex_;
+    map<string,bool> validCmds_; // Solo necesito una hash list, ni siquiera un mapa...
+
+    vector<string> cmdHistory_; // History of command line shell
+    uint cmdIndex_; // Index of current command line
+    uint popIndex_; // Index of popped command from history
     uint pageStep_;
     
     static const char BELL = '\07';
     static const char END_OF_LINE = '\n';
     static const char SPECIAL_KEY = 27;
     static const char BACKSPACE = 127;
+    static const char TAB = 9;
     enum Key { UP = 0, DOWN, RIGHT, LEFT, HOME, END, PGUP, PGDOWN, INS, DEL, OTHER };
 
     int getch();
-    void acceptCmd();
-    void handleSpecialKey();
+    bool acceptLine(); // Accept line after hitting RETURN/INTRO key
+    void handleSpecialKey(); // Like Arrows, PgUp, PgDw, etc..
     void insertChar(char c);
     void eraseChar();
     void renderLine(uint adjust);
-    void eraseCommand();
-    void showCommand();
+    void eraseLine();
+    void popLine();
     Key getKeyCode();
+
+    void checkCommad(string& line);
+
+    static void signal_handler(int signal) {
+        //cout << "\nCaught ^C\n";
+    }
 };
 }
 
 using namespace ase;
+
+void InteractiveShell::init()
+{
+    signal(SIGINT, &signal_handler);
+    validCmds_["ls"] = true;
+    validCmds_["mkdir"] = true;
+    validCmds_["cd"] = true;    
+}
 
 void InteractiveShell::run(void)
 {
@@ -59,12 +80,15 @@ void InteractiveShell::run(void)
     uint adjust = 0;
     while ( (c = getch()) ) {
 
-        //cout << endl << "c:" << ( c > 0 ? (uint) c : (0x100 + (uint)c) ); // debug
+        // Uncomment the 3 cout ins this file to view var values
+        //cout << endl << endl << "c:" << ( c > 0 ? (uint) c : (0x100 + (uint)c) ); // debug
         if (c == END_OF_LINE) {
-            acceptCmd();
+            bool isOk = acceptLine();
+            if (!isOk) {
+                return;
+            }
         }
         else if (c == SPECIAL_KEY) {
-
             // Warning: more characters follow!.
             handleSpecialKey();
         }
@@ -78,6 +102,13 @@ void InteractiveShell::run(void)
                 linePos_--;
                 adjust = 1;
             }
+        }
+        else if (c == TAB) {
+            string line;
+            for (uint i = 0; i < line_.size(); i++) {
+                line.push_back(line_[i]);
+            }
+            checkCommad(line);
         }
         else {
             if (linePos_ == line_.size()) {
@@ -96,7 +127,7 @@ void InteractiveShell::run(void)
     }
 }
 
-void InteractiveShell::acceptCmd()
+bool InteractiveShell::acceptLine()
 {
     // cout << " | acceptCmd()"; // debug
 
@@ -106,8 +137,8 @@ void InteractiveShell::acceptCmd()
     }
     cmdHistory_.push_back(line);
 
-    cout << endl << "command:" << line << " | cmdIdx:" << cmdIndex_ << " | size:" ;
-    cout << cmdHistory_[cmdIndex_].size() << " | rowIdx:" << rowIndex_ << endl;
+    cout << endl << "line:" << line << " | size:" << cmdHistory_[cmdIndex_].size();
+    cout << " | cmdIdx:" << cmdIndex_ <<  " | rowIdx:" << popIndex_ << endl;
     
     for (uint i = 0; i < cmdHistory_.size(); i++) {
         cout << i << ":" << cmdHistory_[i] << endl;
@@ -116,7 +147,23 @@ void InteractiveShell::acceptCmd()
     linePos_ = 0;
     line_.clear();
     cmdIndex_++;
-    rowIndex_ = cmdIndex_;
+    popIndex_ = cmdIndex_;
+
+    if (!line.compare("q")) {
+        return false;
+    }
+    
+    checkCommad(line);
+    return true;
+}
+
+void InteractiveShell::checkCommad(string& line)
+{
+    map<string,bool>::iterator ii;
+    ii = validCmds_.find(line);
+    if (ii != validCmds_.end()) {
+        cout << "Valid command" << endl;
+    }
 }
 
 void InteractiveShell::insertChar(char c)
@@ -170,23 +217,23 @@ void InteractiveShell::handleSpecialKey()
     Key key = getKeyCode();
     switch (key) {
     case UP:
-        if (rowIndex_ == 0) {
+        if (popIndex_ == 0) {
             cout << BELL;
             return;
         }
-        eraseCommand();
-        rowIndex_--;
-        showCommand();
+        eraseLine();
+        popIndex_--;
+        popLine();
         break;
 
     case DOWN:
-        if ( (rowIndex_ >= cmdIndex_ - 1) || cmdIndex_ == 0 ) {
+        if ( (popIndex_ >= cmdIndex_ - 1) || cmdIndex_ == 0 ) {
             cout << BELL;
             return;
         }
-        eraseCommand();
-        rowIndex_++;
-        showCommand();
+        eraseLine();
+        popIndex_++;
+        popLine();
         break;
 
     case RIGHT: {
@@ -214,23 +261,23 @@ void InteractiveShell::handleSpecialKey()
         break;
 
     case PGUP:
-//        if (rowIndex_ - pageStep_ <= 0) {
+//        if (popIndex_ - pageStep_ <= 0) {
 //            cout << BELL;
 //            return;
 //        }
-//        eraseCommand();
-//        rowIndex_ -= pageStep_;
-//        showCommand();
+//        eraseLine();
+//        popIndex_ -= pageStep_;
+//        popLine();
         break;
 
     case PGDOWN:
-//        if (rowIndex_ + pageStep_ >= cmdIndex_) {
+//        if (popIndex_ + pageStep_ >= cmdIndex_) {
 //            cout << BELL;
 //            return;
 //        }
-//        eraseCommand();
-//        rowIndex_ += pageStep_;
-//        showCommand();
+//        eraseLine();
+//        popIndex_ += pageStep_;
+//        popLine();
         break;
 
     case INS:
@@ -270,7 +317,7 @@ InteractiveShell::Key InteractiveShell::getKeyCode()
     else return OTHER;
 }
 
-void InteractiveShell::eraseCommand()
+void InteractiveShell::eraseLine()
 {
     for (uint i = 0; i < line_.size(); i++) {
         cout << "\b \b";
@@ -278,9 +325,9 @@ void InteractiveShell::eraseCommand()
     line_.clear();
 }
 
-void InteractiveShell::showCommand()
+void InteractiveShell::popLine()
 {
-    string line = cmdHistory_[rowIndex_];
+    string line = cmdHistory_[popIndex_];
     for (uint i = 0; i < line.size(); i++) {
         line_.push_back(line[i]);
     }
@@ -323,6 +370,7 @@ int InteractiveShell::getch(void)
 int main()
 {
     InteractiveShell shell;
+    shell.init();
     shell.run();
 
     return 0;
